9119f7a8b5dec6e8105c18aa798ae7a0de404cc9
aiandrox <aiandrox7@gmail.com>
2023-08-11 23:20:58 +0900

    匿名認証用のメソッドを追加 https://blog.aiandrox.com/posts/tech/2021/08/20/

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,32 @@
 #  updated_at  :datetime         not null
 #
 class User < ApplicationRecord
+  attr_accessor :uuid
+
+  class << self
+    def create_anonymous!
+      uuid = new_uuid
+      user = new(
+        uuid: uuid,
+        user_digest: digest(uuid)
+      )
+      user.save!
+      user
+    end
+
+    private
+
+    def new_uuid
+      SecureRandom.urlsafe_base64
+    end
+
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+  end
+
+  def authenticated?(uuid)
+    BCrypt::Password.new(user_digest) == uuid
+  end
 end
