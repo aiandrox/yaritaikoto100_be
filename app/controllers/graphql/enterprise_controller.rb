215bdf9a1c3f9faae0b9558ccf4f1c05e7695254
aiandrox <aiandrox7@gmail.com>
2023-08-12 18:54:48 +0900

    ローカルからGraphQLを叩けるようにした

--- app/controllers/graphql/enterprise_controller.rb
+++ app/controllers/graphql/enterprise_controller.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-class GraphqlController < ApplicationController
+class Graphql::EnterpriseController < ApplicationController
   # If accessing from outside this domain, nullify the session
   # This allows for outside API access while preventing CSRF attacks,
   # but you'll have to authenticate your user separately
   # protect_from_forgery with: :null_session
+  include ::ActionController::Cookies
 
   def execute
     variables = prepare_variables(params[:variables])
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
     post 'enterprise', to: 'enterprise#execute'
   end
 
-  post '/graphql', to: 'graphql#execute'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
